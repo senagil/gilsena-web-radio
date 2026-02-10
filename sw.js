17afe229c1a747e6d3f5c124a8b0ef98a2e49d54
Gilceleno Sena <82612204+senagil@users.noreply.github.com>
2026-02-10 08:36:12 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'gilsena-v1';
+const assets = [
+  '/',
+  '/index.html',
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Resposta às requisições
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
