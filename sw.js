e1841368f1eb0d9adfdb1f348a76aedd76ca39a3
Gilceleno Sena <82612204+senagil@users.noreply.github.com>
2026-02-13 19:07:38 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,37 @@
 const CACHE_NAME = 'gilsena-v1';
 const assets = [
-  '/',
-  '/index.html',
+  './',
+  './index.html',
+  './LOGO RADIO.png'
 ];
 
-// Instalação do Service Worker
-self.addEventListener('install', event => {
-  event.waitUntil(
+self.addEventListener('install', e => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
     })
   );
 });
 
-// Resposta às requisições
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
     })
   );
 });
+
+/* Impede que o usuário selecione textos ou arraste imagens como num site */
+body {
+    user-select: none;
+    -webkit-tap-highlight-color: transparent;
+    touch-action: manipulation;
+}
+
+/* Garante que o app ocupe toda a tela no celular sem barras brancas */
+html, body {
+    height: 100%;
+    position: fixed;
+    overflow: hidden;
+    width: 100%;
+}
